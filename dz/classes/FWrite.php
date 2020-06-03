c4e2f92abaea91434f657f15ffc5d416d085627a
Vasiliy <kkolyasev1228@gmail.com>
2020-06-03 03:28:49 +0300

    updateFile

--- dz/classes/FWrite.php
+++ dz/classes/FWrite.php
@@ -7,11 +7,24 @@ class FWrite {
     public function __construct($name)
     {
         $this->name = $name;
-        $this->file = fopen($name, 'w');
+        $this->file = fopen($name, 'r');
     }
 
     public function writeFile ($text) {
         fwrite($this->file, $text);
+    }
+
+    public function readFile () {
+        return file_get_contents($this->name);
+    }
+
+    public function updateFile ($text) {
+        $len = preg_replace('/\s/', "    ", $text);
+        fclose($this->file);
+        unlink($this->name);
+        $this->name = $this->name . '1';
+        $this->file = fopen($this->name, 'w');
+        fwrite($this->file, $len);
     }
 
     public function __destruct()
--- dz/index.php
+++ dz/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'classes/FWrite.php';
 
-$text = 'Loremlfdldnf';
-$article = new FWrite('name.txt');
-$article->writeFile($text);
+
+$article = new FWrite('1.txt');
+$text = $article->readFile();
+var_dump($text);
+
+$article->updateFile($text);
